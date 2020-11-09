3ed318e943f606d7da4354bbd322af8ad01258c1
Garen Tyler <garentyler@gmail.com>
2020-11-09 09:10:54 -0700

    commit

---
+++ Variables/Variables/Variables.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+	char character = 'c';
+	int integer = -2;
+	unsigned int unsigned_integer = 4;
+	float single_precision_floating_point = 6.3;
+	double double_precision_floating_point = 9.2;
+	bool boolean = false;
+
+	std::cout << character << "\n";
+	std::cout << integer << "\n";
+	std::cout << unsigned_integer << "\n";
+	std::cout << single_precision_floating_point << "\n";
+	std::cout << double_precision_floating_point << "\n";
+	std::cout << boolean << "\n";
+}
